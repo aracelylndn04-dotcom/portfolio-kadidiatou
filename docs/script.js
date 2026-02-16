8b56f60513f9e19015691c55737b36c444e455db
khadija <aracelylndn04@gmail.com>
2026-02-16 20:47:58 +0100

    mise à jour

--- docs/script.js
+++ docs/script.js
@@ -11,3 +11,27 @@ function envoyerMessage() {
   resultat.textContent =
     " Merci " + nom + " ! Ton message a bien été noté. Je te répondrai dès que possible ";
 }
+function ajouterAvis() {
+  const nom = document.getElementById("avisNom").value;
+  const texte = document.getElementById("avisTexte").value;
+  const message = document.getElementById("messageAvis");
+  const liste = document.getElementById("listeAvis");
+
+  if (nom === "" || texte === "") {
+    message.innerText = "Veuillez remplir les champs.";
+    message.style.color = "red";
+    return;
+  }
+
+  const avis = document.createElement("div");
+  avis.className = "avis-item";
+  avis.innerHTML = `<strong>${nom}</strong><p>${texte}</p>`;
+
+  liste.prepend(avis);
+
+  message.innerText = "Merci d’avoir laissé un avis sur mon portfolio et d’avoir pris le temps de visiter.Au plaisir d'être parmis vous et d’échanger avec vous."
+  message.style.color = "green";
+
+  document.getElementById("avisNom").value = "";
+  document.getElementById("avisTexte").value = "";
+}
